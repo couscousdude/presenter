512f0b1e0d2c65c4df63cb4c39375cc81a5538c9
Youwen Wu <youwenw@gmail.com>
2024-03-21 03:32:55 -0700

    feat: add the rest of the slides

--- src/lib/slides.ts
+++ src/lib/slides.ts
@@ -1,3 +1,29 @@
-const slides: string[] = ['Slide1', 'Intro', 'Slide2', 'CorrectLaTeXSubstringColoring']
+const slides: string[] = [
+	'Slide1',
+	'Intro',
+	'Slide2',
+	'Slide3',
+	'Slide4',
+	'Slide5',
+	'Slide6',
+	'Slide7',
+	'Slide8',
+	'Slide9',
+	'Slide9Supplement',
+	'Slide9Supplement2',
+	'Slide10',
+	'Slide11',
+	'Slide12',
+	'Slide13',
+	'Slide14',
+	'Slide15',
+	'Slide16',
+	'Slide17',
+	'Slide18',
+	'Slide19',
+	'Slide20',
+	'Slide21',
+	'Credits'
+]
 
 export default slides
